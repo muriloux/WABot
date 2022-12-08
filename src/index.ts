ccb735263a44480ce50047dfa25249231e2d4fb9
Naoki <murilomelo.code@gmail.com>
2022-12-07 21:21:05 -0300

    creating server for qr code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,53 @@
 import { create, Whatsapp } from "venom-bot";
 import { Commands } from "./commands";
 import { logMessages } from "./services/logMessages";
+import path from "path";
+import express from "express";
 
-create({
-  session: "session-name", //name of session
-})
+const sv = express();
+
+create(
+  "sessionName",
+  (base64Qr, asciiQR, attempts, urlCode) => {
+    console.log(asciiQR); // Optional to log the QR in the terminal
+    var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+      response = {
+        type: "",
+        data: "",
+      };
+
+    if (matches.length !== 3) {
+      return new Error("Invalid input string");
+    }
+    response.type = matches[1];
+    // @ts-ignore
+    response.data = new Buffer.from(matches[2], "base64");
+
+    var imageBuffer = response;
+    require("fs").writeFile(
+      "src/out.png",
+      imageBuffer["data"],
+      "binary",
+      function (err) {
+        if (err != null) {
+          console.log(err);
+        }
+      }
+    );
+
+    sv.get("/qr", (_req, res) => {
+      res.sendFile("out.png", { root: path.join(__dirname) });
+    });
+
+    sv.listen(8081, () => {
+      console.log(
+        "server running on port 8081\ncheck http://localhost:8081/qr for QR code"
+      );
+    });
+  },
+  undefined,
+  { logQR: false }
+)
   .then((client) => {
     start(client);
   })
